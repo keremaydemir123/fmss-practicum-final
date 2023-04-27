08ebb015731e0dfcfbf49d6ed096940424b22d8b
Kerem Aydemir <keremaydemir59@gmail.com>
2023-04-27 17:02:36 +0300

    setup: vite tailwind react-router-dom eslint prettier

---
+++ src/main.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.css';
+
+import HomePage from './pages/HomePage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage />,
+  },
+]);
+
+const root = document.getElementById('root');
+if (root)
+  ReactDOM.createRoot(root).render(
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  );
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,5 @@
+function HomePage() {
+  return <div className="text-red-500">Home Page</div>;
+}
+
+export default HomePage;
