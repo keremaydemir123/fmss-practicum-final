ef2a7d619341ec70d2118fbfbf2565045e35453e
Kerem Aydemir <keremaydemir59@gmail.com>
2023-04-27 21:18:06 +0300

    feat: add LoadMore button

---
+++ src/components/LoadMoreButton.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { useStarships } from '../context/StarshipContext';
+
+function LoadMoreButton(props: React.ComponentPropsWithoutRef<'button'>) {
+  const { nextUrl, getNextStarships } = useStarships();
+  const { children } = props;
+  return (
+    <button
+      onClick={getNextStarships}
+      disabled={!nextUrl}
+      className="btn btn-primary w-max mt-6"
+      {...props}
+    >
+      {nextUrl == null ? 'No more spaceships' : children}
+    </button>
+  );
+}
+
+export default LoadMoreButton;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,8 @@
 import { Card, CardSkeleton } from '../components/Card';
 import CardContainer from '../components/CardContainer';
 import ErrorSection from '../components/ErrorSection';
+import LoadMoreButton from '../components/LoadMoreButton';
+import Loading from '../components/Loading';
 
 import { useStarships } from '../context/StarshipContext';
 import { Starship } from '../types';
@@ -11,9 +13,16 @@ function HomePage() {
   if (error) return <ErrorSection status="500" error={error} />;
 
   return (
-    <CardContainer>
-      {loading ? <Skeletons /> : <Cards starships={starships} />}
-    </CardContainer>
+    <>
+      <CardContainer>
+        {loading && <Loading />}
+        {starships.length == 0 && <Skeletons />}
+        <Cards starships={starships} />
+      </CardContainer>
+      <div className="w-full flex items-center justify-center">
+        <LoadMoreButton>Load More</LoadMoreButton>
+      </div>
+    </>
   );
 }
 
